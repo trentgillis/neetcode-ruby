72a7bbea437332d3b26b51fe245ed7388f468bc7
Trentin Gillis <trentin.gillis@gmail.com>
2022-06-09 22:08:26 -0600

    Solve invert binary tree

---
+++ trees/invert_binary_tree.rb
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  if not root
+    return nil
+  end
+  
+  root.left, root.right = invert_tree(root.right), invert_tree(root.left)
+  root
+end
