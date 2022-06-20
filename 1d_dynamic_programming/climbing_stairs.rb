fdac6be69729af6320250c54968fb578275b0307
Trentin Gillis <trentin.gillis@gmail.com>
2022-06-20 19:23:59 -0400

    Solve climbing stairs with memoized solution

--- 1d_dynamic_programming/climbing_stairs.rb
+++ 1d_dynamic_programming/climbing_stairs.rb
@@ -1,12 +1,26 @@
+# Memoize Solution
 # @param {Integer} n
 # @return {Integer}
 def climb_stairs(n)
-  one, two = 1, 1
-  (2..n).each do
-    temp = one
-    one = one + two
-    two = temp
+  def climb_stairs_memo(n, memo)
+    return memo[n] if memo.include?(n)
+    return 1 if n < 2
+    memo[n] = climb_stairs_memo(n - 1, memo) + climb_stairs_memo(n - 2, memo)
+    memo[n]
   end
   
-  one
+  climb_stairs_memo(n, Hash.new)
+end
+
+# Bottom up solution
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+  dp = []
+  dp[0], dp[1] = 1, 1
+  (2..n).each do |i|
+    dp[i] = dp[i - 1] + dp[i - 2]
+  end
+  
+  dp[n]
 end
