e1104daf2b57c61c26f60816f459be7c197ff156
Trentin Gillis <trentin.gillis@gmail.com>
2022-07-10 00:00:21 -0600

    Solve count_bits via dynamic programming method

--- bit_manipulation/count_bits.rb
+++ bit_manipulation/count_bits.rb
@@ -5,7 +5,7 @@ def count_bits(n)
   result = []
   (0..n).each do |num|
     bin = num.to_s(2)
-    result << count_ones(bin)
+    result.push(count_ones(bin))
   end
 
   result
@@ -14,12 +14,21 @@ end
 def count_ones(bin)
   count = 0
 
-  puts "Start bin: #{bin}"
-
   until bin.empty?
     count += bin.to_i & 1
     bin = bin.chop
   end
 
   count
+end
+
+# Dynamic Programming Solution
+def count_bits(n)
+  result = [0]
+
+  (1..n).each do |num|
+    result[num] = result[num / 2] + (num & 1)
+  end
+
+  result
 end
